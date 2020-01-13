e47f4fa056526ce874cd88ea373f291a4500dfea
Maciej Kaczanowski <maciej.emil.kaczanowski@gmail.com>
2020-01-13 23:38:35 +0100

    v0.3.0 General style guide
     .gitignore update

--- src/apps/exchangeRates/utils/flagSelector/index.tsx
+++ src/apps/exchangeRates/utils/flagSelector/index.tsx
@@ -1,27 +1,29 @@
-import * as React from 'react';
-import { Icons } from '../../../theme/interfaces';
-import { icons } from '../../../theme';
-import { StandardProps } from '@material-ui/core';
+// import * as React from 'react';
+// import { Icons } from '../../../theme/interfaces';
+// import { icons } from '../../../theme';
+// import { StandardProps } from '@material-ui/core';
+//
+// interface Props extends StandardProps<any, any> {
+//   iconName: keyof Icons;
+// }
+//
+// class AppIcon extends React.Component<Props> {
+//   public render(): JSX.Element | string {
+//     const { props } = this;
+//     const icon = icons[props.iconName];
+//
+//     if (!icon) {
+//       return ' ';
+//     }
+//
+//     const newProps = { ...props };
+//
+//     delete newProps.iconName;
+//
+//     return icon.component;
+//   }
+// }
+//
+// export default AppIcon;
 
-interface Props extends StandardProps<any, any> {
-  iconName: keyof Icons;
-}
-
-class AppIcon extends React.Component<Props> {
-  public render(): JSX.Element | string {
-    const { props } = this;
-    const icon = icons[props.iconName];
-
-    if (!icon) {
-      return ' ';
-    }
-
-    const newProps = { ...props };
-
-    delete newProps.iconName;
-
-    return icon.component;
-  }
-}
-
-export default AppIcon;
+export const temmProps = [];
